7003342833533332d080586ce6a2702bfa658773
panktibardolia119933 <pbardolia@albany.edu>
2020-06-27 01:29:50 -0400

    Removed /build file from gitignore

---
+++ build/precache-manifest.3b6e78a3031c88fd6731537994200ffd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6242796fb348e4653915a5c5950afe91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5adc960a7482dff61201",
+    "url": "/static/css/2.f3fc05bd.chunk.css"
+  },
+  {
+    "revision": "6580bd19c8a03f594e6e",
+    "url": "/static/css/main.d5cd94cc.chunk.css"
+  },
+  {
+    "revision": "5adc960a7482dff61201",
+    "url": "/static/js/2.1609ae05.chunk.js"
+  },
+  {
+    "revision": "4cdc9c1fb2bc5dd42a6605dc21c35673",
+    "url": "/static/js/2.1609ae05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6580bd19c8a03f594e6e",
+    "url": "/static/js/main.25d5049d.chunk.js"
+  },
+  {
+    "revision": "363efe348758bce8e69a",
+    "url": "/static/js/runtime-main.ac11ec17.js"
+  }
+]);
