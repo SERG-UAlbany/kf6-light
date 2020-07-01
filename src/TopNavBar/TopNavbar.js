d1427ead75078f578a3214cb6b1f8f6ce074b2d9
Gabriel Falcones <gafalcon@espol.edu.ec>
2020-07-01 19:53:35 -0400

    fix default view on navbar select

--- src/TopNavBar/TopNavbar.js
+++ src/TopNavBar/TopNavbar.js
@@ -44,7 +44,7 @@ class TopNavbar extends Component {
                 <Form className="mrg-1-top">
                   <Col>
                     <FormGroup>
-                      <Input type="select" name="viewId" value={this.props.view.viewId} onChange={this.handleChange}>
+                      <Input type="select" name="viewId" value={this.props.view._id} onChange={this.handleChange}>
                         {
                           this.props.views.map((obj) => {
                             return <option key={obj._id} value={obj._id}> {obj.title} </option>
